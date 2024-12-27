be5638895fbd6d50022d2d05068d4ec839a09c20
nurilamr <nuril0612@gmail.com>
2024-12-28 03:54:23 +0700

    Update koneksi.php

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -5,8 +5,8 @@ error_reporting(0);
 //Database Configuration
 $db_name 	= "psb";
 $host		= "localhost";
-$username	= "root";
-$password	= "";
+$username	= "nuril";
+$password	= "Nuril#1234";
 
 //make connection to database
 $conn 		= mysqli_connect($host,$username,$password,$db_name) or die("Database connection error!");
